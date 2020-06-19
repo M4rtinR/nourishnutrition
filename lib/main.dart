61302d7ba440e8a850bc41bd9a83b0b4f8fb3458
Martin Ross <36231919+M4rtinR@users.noreply.github.com>
2020-06-19 18:03:31 +0100

    Added analytics event and modified analytics class

--- lib/main.dart
+++ lib/main.dart
@@ -8,54 +8,64 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   static FirebaseAnalytics analytics = FirebaseAnalytics();
-  final MyAnalytics _myAnalytics = MyAnalytics();
+  final MyAnalytics _myAnalytics = MyAnalytics(analytics: analytics);
 
   @override
   Widget build(BuildContext context) {
-    _myAnalytics.sendAppOpenEvent();
     return MaterialApp(
       title: 'Nourish Nutrition',
-      home: MyHomePage(),
+      home: MyHomePage(
+        title: 'Nourish Nutrition',
+        myAnalytics: _myAnalytics,
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  MyHomePage({this.title, this.myAnalytics});
+
+  final String title;
+  final MyAnalytics myAnalytics;
 
   @override
   _MyHomePageState createState() {
-    return _MyHomePageState();
+    return _MyHomePageState(myAnalytics);
   }
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  _MyHomePageState(this.myAnalytics);
+
+  final MyAnalytics myAnalytics;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Baby Name Votes')),
-      body: _buildBody(context),
+      appBar: AppBar(title: Text(widget.title)),
+      body: _buildBody(context, myAnalytics),
     );
   }
 
-  Widget _buildBody(BuildContext context) {
+  Widget _buildBody(BuildContext context, MyAnalytics _myAnalytics) {
     return StreamBuilder<QuerySnapshot>(
       stream: Firestore.instance.collection('habits').snapshots(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) return LinearProgressIndicator();
 
-        return _buildList(context, snapshot.data.documents);
+        return _buildList(context, snapshot.data.documents, _myAnalytics);
       },
     );
   }
 
-  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot, MyAnalytics _myAnalytics) {
     return ListView(
       padding: const EdgeInsets.only(top: 20.0),
-      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+      children: snapshot.map((data) => _buildListItem(context, data, _myAnalytics)).toList(),
     );
   }
 
-  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+  Widget _buildListItem(BuildContext context, DocumentSnapshot data, MyAnalytics _myAnalytics) {
     final record = Record.fromSnapshot(data);
 
     return Padding(
@@ -69,7 +79,11 @@ class _MyHomePageState extends State<MyHomePage> {
         child: ListTile(
           title: Text(record.name),
           trailing: Text(record.iconName),
-          onTap: () => print(record),
+          onTap: () {
+            print(record);
+            var thisName = record.name;
+            _myAnalytics.sendAnalyticsEvent("tileTap", "User tapped tile $thisName");
+          },
         ),
       ),
     );
